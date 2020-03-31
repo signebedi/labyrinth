cc7a916098fae9550c022bf3a3a0dd5606e40f7e
signebedi <signe@siftingwinnowing.com>
2020-03-31 13:57:55 -0500

    updates to labyrinth.py, updated Twisted

--- labyrinth.py
+++ labyrinth.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python
 
-player = {}
-player['hp'] = 10
-player['lvl'] = 1
-player['str'] = 1
-player['int'] = 1
-player['con'] = 1
-player['is_alive'] = True
+import json
 
-player['name'] = input('\nWhat is your name?\n> ')
+header = [
+    '  _           _                _       _   _     ',
+    ' | |         | |              (_)     | | | |    ',
+    ' | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  ',
+    " | |    / _` | '_ \| | | | '__| | '_ \| __| '_ \ ",
+    ' | |___| (_| | |_) | |_| | |  | | | | | |_| | | |',
+    ' |______\__,_|_.__/ \__, |_|  |_|_| |_|\__|_| |_|',
+    '                     __/ |                       ',
+    '                    |___/                        '
+]
 
-print(f"\nWelcome to Labyrinth, {player['name']}!\n\nThe evil king MINOS as invaded the ancient city of ATHENS...\n") # Agni will write the rest of the back story for this section
+for item in header: print(item)
+
+print("Welcome to LABYRINTH ! The evil king MINOS has invaded the ancient city of ATHENS and enforces his rule through his son, the cruel MINOTAUR ! It is up to YOU to save the city and defeat the evil monster ! But beware... you must traverse the depths of the LABYRINTH to find and defeat your foe. Good luck !")
 
 # if player['input'] == 'help':
 #   print(player['options']) #each GameMode will set different player options
+
+class Player():
+    def __init__(self): # here we start by building all of the player statistics that we would like to use throughout the game
+        self.stats = { # one way we can store Player stats is in a Player.stats dict...
+            'hp':10, 
+            'lvl':1,
+            'str':1,
+            'int':1,
+            'con':1,
+            'gold':5,
+            'is_alive':True
+        }
+        self.hp = 10
+        self.lvl = 1
+        self.str = 1
+        self.int = 1
+        self.con = 1
+        self.gold = 5
+        self.is_alive = True
+        self.name = input('\nWhat is your name?\n> ')
+    def name(self):
+        self
+    def input(self): pass
+    def dead(self):
+        print(f'\nOh dear, {self.name}... it looks like you have been defeated. Better luck next time.\n')
+        self.end_text = [
+            ' ________         ____        __  __',
+            '/_  __/ /  ___   / __/__  ___/ / / /',
+            ' / / / _ \/ -_) / _// _ \/ _  / /_/ ',
+            '/_/ /_//_/\__/ /___/_//_/\_,_/ (_)  \n\n',
+        ]
+        for item in self.end_text:print(item)
+        self.is_alive = False
 
 
 class WalkingMode():
@@ -21,19 +59,26 @@ class WalkingMode():
         self.move=''
         self.test=True
 
+# class EnemyGeneratorMode():
+    # this was an idea I had: why don't we make it an option for a player to permanently generate a new
+    # enemy and to save it to a player_generated_enemies.json file
+
 # class BattleMode():
 
 # class StoreMode():
 
 # class WorkMode():
 
-# def MapGenerator(x=100,y=100): # this is where we will generate the map for each game
-#    pass
+
+player = Player()
+
+print(f"\nWelcome to Labyrinth, {player.name}!\n") 
+
 
 def main():
-    while player['is_alive']:
-        player['input'] = input('\nWhat would you like to do next?\n> ')
-        print(player['input'])
+    while player.is_alive:
+        i = input('\nWhat would you like to do next?\n> ')
+        print(i)
 
 if __name__ == '__main__':
     main()
---
+++ map.py
@@ -0,0 +1,14 @@
+# class MapGenerator(x=100,y=100): # this is where we will generate the map for each game
+    # def __init__(self):
+        # here we want to build a map similar to the one below that 
+        # is callable using positional notation.
+# map = [
+# 'x xxxxxxxxx x',
+# 'x      xxxx x',
+# 'xxxx   x xx x',
+# 'xxxxxx x    x',
+# 'xxxx x x xxxx',
+# 'xxxx x x xxxx',
+# 'xxxx   x xxxx'
+# ]
+# print(map[0][2]) # this will return 'x', which we can incorporate into our movement logic
